18dfc449d1b72ecb4e5dca89bde87472a40d6171
Wout <wout@meetmatch.biz>
2025-08-30 10:14:14 +0200

    Made auth layout responsive

--- resources/js/layouts/AuthLayout.tsx
+++ resources/js/layouts/AuthLayout.tsx
@@ -2,9 +2,9 @@ import { Outlet } from "react-router-dom";
 
 export default function AuthLayout(){
     return (
-        <div className="min-h-screen flex">
+        <div className="min-h-screen flex flex-col lg:flex-row">
             <div className="w-1/2 bg-base-300"></div>
-            <div className="w-1/2 flex justify-center items-center">
+            <div className="w-full lg:w-1/2 grow flex justify-center items-center">
                 <Outlet/>
             </div>
         </div>
