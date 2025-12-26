f960f27a082e0f44cfc52c55b2b55395ec61a361
woutvw <woutvanwonterghem@gmail.com>
2025-12-26 11:42:53 +0100

    More fixes to vite config

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ['resources/js/app.tsx','resources/css/app.css'],
+            input: ['resources/js/App.tsx','resources/css/app.css'],
             refresh: true,
         }),
         react(),
